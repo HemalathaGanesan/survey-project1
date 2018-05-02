4777bf3e574923c8c164f94c54438a1a87b6d461
Daka Raja <daka.raja@mountblue.io>
2018-05-02 16:07:35 +0530

    register and login form pages created with validation and with confirmation mail

--- server/route/api.js
+++ server/route/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
+const bcrypt = require('bcrypt');
 
 // get request
 router.get('/', (req, res) => {
@@ -8,51 +9,59 @@ router.get('/', (req, res) => {
 });
 
 // post request
-router.post('/login', (req, res) => {
+router.post('/registration', (req, res) => {
   const data = req.body;
   // console.log(data);
-  
+
+  //encrypt password using bcrypt
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(data.password, salt, function (err, hash) {
+      // Store hash in your password DB.
+      // console.log(hash)
+    });
+  });
+
   // const url = `http://localhost:3000/confirmation/${emailToken}`;
-  const url = `http://localhost:3000/login/`;
-  const mailAccount = {
-    user: 'test.skiploop@gmail.com',
-    password: 'Test@123',
-  }
+  // const url = `http://localhost:3000/login/`;
+  // const mailAccount = {
+  //   user: 'test.skiploop@gmail.com',
+  //   password: 'Test@123',
+  // }
 
-  // send confirmation mail
-  nodemailer.createTestAccount((err, account) => {
-    let transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
-      auth: {
-        user: mailAccount.user,
-        pass: mailAccount.password
-      },
-      // tls: {
-      //   rejectUnauthorized: false
-      // }
-    });
+  // // send confirmation mail
+  // nodemailer.createTestAccount((err, account) => {
+  //   let transporter = nodemailer.createTransport({
+  //     host: 'smtp.gmail.com',
+  //     port: 587,
+  //     secure: false,
+  //     auth: {
+  //       user: mailAccount.user,
+  //       pass: mailAccount.password
+  //     },
+  //     // tls: {
+  //     //   rejectUnauthorized: false
+  //     // }
+  //   });
 
-    let mailOptions = {
-      from: `"Confirm mail" <${account.user}>`,
-      to: data.email,
-      subject: 'Confirm Email',
-      // text: 'Hello, click below link to confirm !!',
-      html: `hello!! Please click this link to confirm your email: <a href="${url}">${url}</a>`,
-    };
+  //   let mailOptions = {
+  //     from: `"Confirm mail" <${account.user}>`,
+  //     to: data.email,
+  //     subject: 'Confirm Email',
+  //     // text: 'Hello, click below link to confirm !!',
+  //     html: `hello!! Please click this link to confirm your email: <a href="${url}">${url}</a>`,
+  //   };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        return console.log(error);
-      }
-      console.log('Message sent: %s', info.messageId);
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  //   transporter.sendMail(mailOptions, (error, info) => {
+  //     if (error) {
+  //       return console.log(error);
+  //     }
+  //     console.log('Message sent: %s', info.messageId);
+  //     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    });
-  })
+  //     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  //     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  //   });
+  // })
 });
 
 module.exports = router;
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -6,8 +6,13 @@ import { Link } from 'react-router-dom'
 class Registration extends Component {
   constructor() {
     super();
-    this.state = { isMailSent: false }
+    this.state = {
+      isMailSent: false,
+      isValidPass: false,
+      isValidConfPass: false,
+    }
   }
+
   storeData(e) {
     e.preventDefault();
     let data = {
@@ -16,6 +21,14 @@ class Registration extends Component {
       "hospital": this.refs.hospital.value
     }
     // console.log(data)
+  
+    if (this.refs.confirmPassword.value.length < 8 || (this.refs.password.value !== this.refs.confirmPassword.value)) {
+      this.setState({ isValidConfPass: true })
+    } else {
+      this.setState({ isValidConfPass: false });
+      this.postReq(data);
+    }
+
     this.props.dispatch({
       type: 'REGISTRATION_DATA',
       payload: {
@@ -24,8 +37,10 @@ class Registration extends Component {
         hospital: this.refs.hospital.value
       }
     })
-
-    fetch("http://localhost:3001/api/login/", {
+  }
+  postReq(data) {
+    console.log(data)
+    fetch("http://localhost:3001/api/registration/", {
       body: JSON.stringify(data),
       headers: new Headers({
         'Content-Type': 'application/json'
@@ -36,9 +51,38 @@ class Registration extends Component {
       .catch(error => console.log(error));
 
     this.setState({ isMailSent: true })
-    setTimeout(() => this.setState({ isMailSent: false }), 5000)
-
+    setTimeout(() => this.setState({ isMailSent: false }), 5000);
     this.refs.registrationForm.reset();
+  }
+
+
+  validatePass(e) {
+    console.log(this.refs.password.value);
+    if(this.refs.password.value === ''){
+      this.setState({ isValidPass1: false, isValidPass2: false, isValidPass3: false });
+    } else if (this.refs.password.value.length < 8) {
+      this.setState({
+        isValidPass1: true, isValidPass2: false, isValidPass3: false
+      })
+    } else if (!this.refs.password.value.match(/[A-Z]/)) {
+      this.setState({
+        isValidPass2: true, isValidPass3: false, isValidPass1: false
+      })
+    } else if (!this.refs.password.value.match(/[0-9]/)) {
+      this.setState({
+        isValidPass3: true, isValidPass1: false, isValidPass2: false
+      })
+    } else {
+      this.setState({
+        isValidPass1: false, isValidPass2: false, isValidPass3: false
+      })
+    }
+  }
+  validateConfPass(e) {
+    console.log(this.refs.confirmPassword.value)
+    if(this.refs.confirmPassword.value !== ''){
+      this.setState({ isValidConfPass: false });
+    } else this.setState({ isValidConfPass: false });
   }
 
   render() {
@@ -63,15 +107,30 @@ class Registration extends Component {
                   <div className="form-group row">
                     <label htmlFor="password" className="col-sm-4 col-form-label required">Password</label>
                     <div className="col-sm-8">
-                      <input type="password" className="form-control" id="password" ref="password" placeholder="Password" required />
-                      <small><p className="text-left">password should atleast 8 characters</p></small>
+                      <input type="password" className="form-control" id="password" ref="password" placeholder="Password" required onChange={this.validatePass.bind(this)} />
+                      {/* <small><p className="text-left">password should atleast 8 characters</p></small> */}
+                      {/* {this.state.isValidPass && <div className="alert alert-danger alert-dismissible fade show text-left" role="alert">
+                        <strong>Password must have</strong>
+                        <ul>
+                          <li>atleast 8 characters</li>
+                          <li>1 uppercase(A-Z)</li>
+                          <li>1 number(0-9)</li>
+                        </ul>
+                        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>} */}
+                      {this.state.isValidPass1 && <div className="alert alert-danger" role="alert">password must atleast 8 characters!</div>}
+                      {this.state.isValidPass2 && <div className="alert alert-danger" role="alert">1 uppercase!</div>}
+                      {this.state.isValidPass3 && <div className="alert alert-danger" role="alert">1 number!</div>}
                     </div>
                   </div>
 
                   <div className="form-group row">
                     <label htmlFor="confirmPassword" className="col-sm-4 col-form-label required">Confirm Password</label>
                     <div className="col-sm-8">
-                      <input type="password" className="form-control" id="confirmPassword" placeholder="Confirm Password" required />
+                      <input type="password" className="form-control" id="confirmPassword" ref="confirmPassword" placeholder="Confirm Password" required onChange={this.validateConfPass.bind(this)} />
+                      {this.state.isValidConfPass && <div className="alert alert-danger" role="alert">password didn't match!</div>}
                     </div>
                   </div>
 
@@ -87,7 +146,7 @@ class Registration extends Component {
               </div>
             </div>
             <span>Already have account? Login <Link to="/login"> Here</Link></span>
-            {this.state.isMailSent && <div className="alert alert-success" role="alert">Mail has been sent to your mail</div>}
+            {this.state.isMailSent && <div className="alert alert-success" role="alert">Confirmation mail has been sent to your mail</div>}
           </div>
           <div className="col"></div>
         </div>
